6967932207740223416cc5e56aad58c4d5e26c7c
wholesomeow <drcappuchino2@gmail.com>
2025-02-11 00:27:51 -0700

    Added stubs to NPC struct

--- npc/npcBase.go
+++ npc/npcBase.go
@@ -6,8 +6,36 @@ import (
 
 // TODO(wholesomeow): Implement UUIDs based on RFC 4122 and DCE 1.1: Authentication and Security Services
 type NPCBase struct {
+	// v0.1
 	UUID int
 	Name string
+	// OCEAN
+	// Enneagram
+	// MICE
+	// CS
+	// REI
+
+	// v0.2
+	// Type
+	// Race
+	// Sex
+	// Gender
+	// Pronouns
+	// Sexual Orientation
+	// Body Type
+	// Height and Weight
+
+	// v1.0
+	// Social Role
+	// Communication Matrix
+	// Social Circle
+
+	// v1.1
+	// Physical Description
+
+	// v1.2
+	// Rumors Known
+	// Jobs Known
 }
 
 func DisplayName(npc NPCBase) {
