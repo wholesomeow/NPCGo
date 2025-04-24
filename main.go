f8e268d7ff936092610fa1e990bba65ead422853
wholesomeow <drcappuchino2@gmail.com>
2025-04-23 18:23:07 -0600

    Initial gin REST API implementation

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"time"
-
-	config "github.com/wholesomeow/npcGo/configs"
-	npcgen "github.com/wholesomeow/npcGo/pkg/npcGen"
+	"github.com/gin-gonic/gin"
+	npcapi "github.com/wholesomeow/npcGo/pkg/npcGen/api"
 )
 
 func main() {
-	// Read in Database Config file
-	config, err := config.ReadConfig("configs/dbconf.yml")
-	if err != nil {
-		log.Fatal(err)
-	}
+	router := gin.Default()
 
-	// Create NPC
-	start_proc := time.Now()
-	npc_object, err := npcgen.CreateNPC(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	end_proc := time.Now()
-	elapsed_proc := end_proc.Sub(start_proc)
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
 
-	fmt.Println("----- OUTPUT -----")
+	npc_group := router.Group("/npc")
+	npc_group.GET("/createNPC", npcapi.APICreateNPC)
 
-	fmt.Println(npc_object.DataToJSON())
-	// fmt.Println(npc_object.OCEAN.Text)
-	log.Printf("npc created... elapsed time: %s", time.Duration.String(elapsed_proc))
+	router.Run("0.0.0.0:8080")
 }
