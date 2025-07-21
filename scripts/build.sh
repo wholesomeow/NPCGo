e17e61a2e1e89cafe73c2544afd430696d487606
wholesomeow <drcappuchino2@gmail.com>
2025-07-21 02:23:57 -0600

    Refactor build script: Remove unused test build options and streamline build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,19 +3,14 @@
 if [ -z $1 ]; then
   echo "Must provide build mode as argument"
   echo "Options are:"
-  echo "  - main-build          Builds and runs the main binary"
-  echo "  - main-test           Builds the main binary and runs its tests"
-  echo "  - dev-build           Builds and runs the devtools binary"
-  echo "  - dev-test            Builds the devtools binary and runs its tests"
+  echo "  - main-build          Builds the main binary and runs its tests"
+  echo "  - dev-build           Builds the devtools binary and runs its tests"
+  echo "  - release-build       Builds the main binary for windows and linux"
   exit 1
 fi
 
 case $1 in
   main-build )
-    echo "Building NPC Generator Binary"
-    go build -o npcgen ./cmd
-    ;;
-  main-test )
     # Run the tests first so the binary wont build if tests fail
     echo "Running NPC Generator tests"
     go test ./...
@@ -24,16 +19,24 @@ case $1 in
     go build -o npcgen ./cmd
     ;;
   dev-build )
-    echo "Building NPC Generator Binary"
-    go build -o devtools ./cmd/devtools
-    ;;
-  dev-test )
     # Run the tests first so the binary wont build if tests fail
     echo "Running NPC Generator Devtools tests"
     go test ./...
 
-    echo "Building NPC Generator Devtools Binary"
+    echo "Building NPC Generator Binary"
     go build -o devtools ./cmd/devtools
+    ;;
+  release-build )
+    # Run the tests first so the binary wont build if tests fail
+    echo "Running NPC Generator tests"
+    go test ./...
+
+    echo "Building NPC Generator Binary"
+    # Linux 64-bit
+    GOOS=linux GOARCH=amd64 go build -o npcgen-linux
+
+    # Windows 64-bit
+    GOOS=windows GOARCH=amd64 go build -o npcgen.exe
     ;;
   
 esac
